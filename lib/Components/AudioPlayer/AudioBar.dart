6879bcb6efb59cfd67b2dc64ecf9e75ca0e7a9b4
DankmonVP <80044878+DankmonVP@users.noreply.github.com>
2024-06-06 12:27:57 +0700

    1.2
    song page

--- lib/Components/AudioPlayer/AudioBar.dart
+++ lib/Components/AudioPlayer/AudioBar.dart
@@ -7,6 +7,7 @@ class AudioAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     return StreamBuilder<PlayerState>(
       stream: player.playerStateStream,
       builder: (context, snapshot) {
@@ -29,6 +30,82 @@ class AudioAppBar extends StatelessWidget {
           return const Icon(Icons.play_arrow, size: 20, color: Colors.white);
         }
       },
+=======
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: player.seekToPrevious,
+          icon: const Icon(Icons.skip_previous),
+          iconSize: 50,
+          color: Colors.white,
+        ),
+        StreamBuilder<PlayerState>(
+          stream: player.playerStateStream,
+          builder: (context, snapshot) {
+            final playerState = snapshot.data;
+            final processingState = playerState?.processingState;
+            final playing = playerState?.playing;
+            if (!(playing ?? false)) {
+              return IconButton(
+                  onPressed: player.play,
+                  iconSize: 50,
+                  color: Colors.white,
+                  icon: const Icon(Icons.play_arrow));
+            } else if (processingState != ProcessingState.completed) {
+              return IconButton(
+                  onPressed: player.pause,
+                  iconSize: 50,
+                  color: Colors.white,
+                  icon: const Icon(Icons.pause));
+            } else {
+              return const Icon(Icons.play_arrow,
+                  size: 50, color: Colors.white);
+            }
+          },
+        ),
+        IconButton(
+          onPressed: player.seekToNext,
+          icon: const Icon(Icons.skip_next),
+          iconSize: 50,
+          color: Colors.white,
+        ),
+        // StreamBuilder<PlayerState>(
+        //   stream: player.playerStateStream,
+        //   builder: (context, snapshot) {
+        //     int loopingState = 0;
+        //     if (loopingState == 1) {
+        //       return IconButton(
+        //           onPressed: () async {
+        //             await player.setLoopMode(LoopMode.all);
+        //             loopingState = 2;
+        //           },
+        //           iconSize: 50,
+        //           color: Colors.white,
+        //           icon: const Icon(Icons.lock));
+        //     } else if (loopingState == 2) {
+        //       return IconButton(
+        //           onPressed: () async {
+        //             await player.setLoopMode(LoopMode.off);
+        //             loopingState = 0;
+        //           },
+        //           iconSize: 50,
+        //           color: Colors.blue,
+        //           icon: const Icon(Icons.loop));
+        //     } else {
+        //       return IconButton(
+        //           onPressed: () async {
+        //             await player.setLoopMode(LoopMode.one);
+        //             loopingState = 1;
+        //           },
+        //           iconSize: 50,
+        //           color: Colors.white,
+        //           icon: const Icon(Icons.loop));
+        //     }
+        //   },
+        // ),
+      ],
+>>>>>>> Stashed changes
     );
   }
 }
--- lib/Pages/audio_detail.dart
+++ lib/Pages/audio_detail.dart
@@ -1,10 +1,15 @@
+<<<<<<< Updated upstream
+=======
+import 'dart:io';
+
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+>>>>>>> Stashed changes
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:music_app/Components/AudioPlayer/AudioBar.dart';
 
 class AudioDetail extends StatefulWidget {
   const AudioDetail({super.key});
-
   @override
   State<AudioDetail> createState() => _AudioDetailState();
 }
@@ -15,8 +20,20 @@ class _AudioDetailState extends State<AudioDetail> {
   @override
   void initState() {
     super.initState();
+<<<<<<< Updated upstream
     _audioPlayer = AudioPlayer()
       ..setUrl("http://localhost:8080/api/v1/send/audio/Panko.mp3");
+=======
+    _audioPlayer = AudioPlayer();
+
+    //Ghep cung nhac
+    _init();
+  }
+
+  Future<void> _init() async {
+    await _audioPlayer.setLoopMode(LoopMode.all);
+    await _audioPlayer.setAudioSource(_playlist);
+>>>>>>> Stashed changes
   }
 
   @override
